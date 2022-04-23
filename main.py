84ef7a849a9a5a96f15e163f541452f6d39e8250
HULI <doddahulugappa@gmail.com>
2022-04-23 15:44:06 +0400

    updated the main.py with rotational logging enabled and added logs for all the endpoints

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 import logging
 import logging.handlers as handlers
@@ -11,7 +12,6 @@ logger.setLevel(logging.INFO)
 
 # Here we define our formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 logHandler = handlers.RotatingFileHandler('api_app.log', maxBytes=10*1024,  backupCount=2)
 logHandler.setLevel(logging.INFO)
 
@@ -27,38 +27,48 @@ async def get_nth_fibonacci_number(number: int):
     """
     returns nth fibonacci number
     """
-    start = timeit.default_timer()
     if number < 0:
-        return {"Message": "Invalid Input"}
+        message = {"Message": "Invalid Input"}
+        return JSONResponse(message)
+    start = timeit.default_timer()
     a, b = 0, 1  # Initial first fibonacci numbers
     for i in range(number):
         a, b = b, a+b
     fibonacci_no = a
     stop = timeit.default_timer()
     logger.info(get_nth_fibonacci_number.__name__+" Called")
-    logger.info("Input: " + str(number))
-    logger.info("Output:" + str(fibonacci_no))
-    logger.info("Time Taken(in seconds):" + str(stop - start))
+    logger.info("Input: {}".format(number))
+    logger.info("Output: {}".format(fibonacci_no))
+    logger.info("Time Taken(in seconds): {}".format(stop - start))
     ord_value = ordinal(number)  # get the ordinal of number
-    message = {ord_value+" Fibonacci Number": fibonacci_no}
-    return message
+    message = {"{} Fibonacci Number".format(ord_value): fibonacci_no}
+    return JSONResponse(message)
 
 
-@app.get("/factorial/{n}")
-async def get_factorial_of_given_number(n: int):
+@app.get("/factorial/{number}")
+async def get_factorial_of_given_number(number: int):
     """
     returns factorial of a given number
     """
-    if n < 0:
-        return {"Message": "Invalid Input"}
+    if number < 0:
+        message = {"Message": "Invalid Input"}
+        return JSONResponse(message)
 
-    if n == 0 or n == 1:
+    start = timeit.default_timer()
+    if number == 0 or number == 1:
         factorial = 1
     else:
         # starts multiply by 1 to till N and saves cumulative product
-        factorial = reduce(lambda x, y: x * y, range(1, n+1))
-    message = {"Factorial of " + str(n): factorial}
-    return message
+        factorial = reduce(lambda x, y: x * y, range(1, number+1))
+
+    stop = timeit.default_timer()
+    logger.info(get_factorial_of_given_number.__name__ + " Called")
+    logger.info("Input: {}".format(number))
+    logger.info("Output: {}".format(factorial))
+    logger.info("Time Taken(in seconds): {}".format(stop - start))
+
+    message = {"Factorial of {}".format(number): factorial}
+    return JSONResponse(message)
 
 
 @app.get("/ackermann/")
@@ -69,12 +79,20 @@ async def solve_ackermann(m: int = 0, n: int = 1):
     :param n:
     :return: solution
     """
+    start = timeit.default_timer()
     try:
         solution = ackermann(m, n)
     except Exception as e:
         print(e)
         solution = "Maximum recursion depth exceeded in comparison, Try M smaller number"
-    return {"Ackermann Solution": solution}
+
+    stop = timeit.default_timer()
+    logger.info(solve_ackermann.__name__ + " Called")
+    logger.info("Input: {},{}".format(m, n))
+    logger.info("Output: {}".format(solution))
+    logger.info("Time Taken(in seconds): {}".format(stop - start))
+    message = {"Ackermann Solution": solution}
+    return JSONResponse(message)
 
 
 def ackermann(m, n):
