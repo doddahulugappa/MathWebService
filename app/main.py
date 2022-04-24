4a4b35993a459c49578f071f56391732e7b9d0f2
HULI <doddahulugappa@gmail.com>
2022-04-24 18:59:09 +0400

    updated logging

--- app/main.py
+++ app/main.py
@@ -27,8 +27,10 @@ async def get_nth_fibonacci_number(number: int):
     """
     returns nth fibonacci number
     """
+    logger.info(get_nth_fibonacci_number.__name__ + " Called")
     if number < 0:
         message = {"Message": "Invalid Input"}
+        logger.warning("Invalid Input")
         return JSONResponse(message)
     start = timeit.default_timer()
     a, b = 0, 1  # Initial first fibonacci numbers
@@ -36,7 +38,6 @@ async def get_nth_fibonacci_number(number: int):
         a, b = b, a+b
     fibonacci_no = a
     stop = timeit.default_timer()
-    logger.info(get_nth_fibonacci_number.__name__+" Called")
     logger.info("Input: {}".format(number))
     logger.info("Output: {}".format(fibonacci_no))
     logger.info("Time Taken(in seconds): {}".format(stop - start))
@@ -50,8 +51,10 @@ async def get_factorial_of_given_number(number: int):
     """
     returns factorial of a given number
     """
+    logger.info(get_factorial_of_given_number.__name__ + " Called")
     if number < 0:
         message = {"Message": "Invalid Input"}
+        logger.warning("Invalid Input")
         return JSONResponse(message)
 
     start = timeit.default_timer()
@@ -62,7 +65,6 @@ async def get_factorial_of_given_number(number: int):
         factorial = reduce(lambda x, y: x * y, range(1, number+1))
 
     stop = timeit.default_timer()
-    logger.info(get_factorial_of_given_number.__name__ + " Called")
     logger.info("Input: {}".format(number))
     logger.info("Output: {}".format(factorial))
     logger.info("Time Taken(in seconds): {}".format(stop - start))
@@ -79,6 +81,7 @@ async def solve_ackermann(m: int = 0, n: int = 1):
     :param n:
     :return: solution
     """
+    logger.info(solve_ackermann.__name__ + " Called")
     start = timeit.default_timer()
     try:
         solution = ackermann(m, n)
@@ -87,7 +90,6 @@ async def solve_ackermann(m: int = 0, n: int = 1):
         solution = "Maximum recursion depth exceeded in comparison, Try M smaller number"
 
     stop = timeit.default_timer()
-    logger.info(solve_ackermann.__name__ + " Called")
     logger.info("Input: {},{}".format(m, n))
     logger.info("Output: {}".format(solution))
     logger.info("Time Taken(in seconds): {}".format(stop - start))
