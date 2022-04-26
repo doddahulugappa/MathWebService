353c373c2433f588d2422b91d4f9bb59a3fe6b7b
HULI <doddahulugappa@gmail.com>
2022-04-26 18:21:54 +0400

    Updated Final version

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from functools import reduce
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
@@ -10,9 +11,10 @@ import timeit
 logger = logging.getLogger('main_app')
 logger.setLevel(logging.INFO)
 
+LOG_FILE = 'api_app.log'
 # Here we define our formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logHandler = handlers.RotatingFileHandler('api_app.log', maxBytes=10 * 1024, backupCount=2)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+logHandler = handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024 * 1024, backupCount=2)  # 1 MB
 logHandler.setLevel(logging.INFO)
 
 # Here we set our logHandler's formatter
@@ -27,10 +29,10 @@ async def get_nth_fibonacci_number(number: int):
     """
     returns nth fibonacci number
     """
-    logger.info(get_nth_fibonacci_number.__name__ + " Called")
     if number < 0:
         message = {"Message": "Invalid Input"}
-        logger.warning("Invalid Input")
+        format_log = "Endpoint: Fibonacci - Input: {} - Output: Invalid Input".format(number)
+        logger.warning(format_log)
         return JSONResponse(message)
     start = timeit.default_timer()
     a, b = 0, 1  # Initial first fibonacci numbers
@@ -38,9 +40,9 @@ async def get_nth_fibonacci_number(number: int):
         a, b = b, a+b
     fibonacci_no = a
     stop = timeit.default_timer()
-    logger.info("Input: {}".format(number))
-    logger.info("Output: {}".format(fibonacci_no))
-    logger.info("Time Taken(in seconds): {}".format(stop - start))
+    format_log = "Endpoint: Fibonacci - Input: {} - Output: {} - Time Taken(in seconds): {}" \
+                 "".format(number, fibonacci_no, stop - start)
+    logger.info(format_log)
     ord_value = ordinal(number)  # get the ordinal of number
     message = {"{} Fibonacci Number".format(ord_value): fibonacci_no}
     return JSONResponse(message)
@@ -51,10 +53,10 @@ async def get_factorial_of_given_number(number: int):
     """
     returns factorial of a given number
     """
-    logger.info(get_factorial_of_given_number.__name__ + " Called")
     if number < 0:
         message = {"Message": "Invalid Input"}
-        logger.warning("Invalid Input")
+        format_log = "Endpoint: Factorial - Input: {} - Output: Invalid Input".format(number)
+        logger.warning(format_log)
         return JSONResponse(message)
 
     start = timeit.default_timer()
@@ -65,9 +67,9 @@ async def get_factorial_of_given_number(number: int):
         factorial = reduce(lambda x, y: x * y, range(1, number+1))
 
     stop = timeit.default_timer()
-    logger.info("Input: {}".format(number))
-    logger.info("Output: {}".format(factorial))
-    logger.info("Time Taken(in seconds): {}".format(stop - start))
+    format_log = "Endpoint: Factorial - Input: {} - Output: {} - Time Taken(in seconds): {}" \
+                 "".format(number, factorial, stop - start)
+    logger.info(format_log)
 
     message = {"Factorial of {}".format(number): factorial}
     return JSONResponse(message)
@@ -81,7 +83,6 @@ async def solve_ackermann(m: int = 0, n: int = 1):
     :param n:
     :return: solution
     """
-    logger.info(solve_ackermann.__name__ + " Called")
     start = timeit.default_timer()
     try:
         solution = ackermann(m, n)
@@ -90,11 +91,32 @@ async def solve_ackermann(m: int = 0, n: int = 1):
         solution = "Maximum recursion depth exceeded in comparison, Try M smaller number"
 
     stop = timeit.default_timer()
-    logger.info("Input: {},{}".format(m, n))
-    logger.info("Output: {}".format(solution))
-    logger.info("Time Taken(in seconds): {}".format(stop - start))
+    format_log = "Endpoint: Ackermann - Input: M={},N={} - Output: {} - Time Taken(in seconds): {}" \
+                 "".format(m, n, solution, stop - start)
+    logger.info(format_log)
     message = {"Ackermann Solution": solution}
     return JSONResponse(message)
+
+
+@app.get("/logs/")
+async def get_logs():
+    log_list = []
+    if os.path.exists(LOG_FILE):
+        with open(LOG_FILE, "r") as of:  # Open log file in read mode
+            logs = of.readlines()  # Read all the lines
+
+        for line in logs:
+            logs_dict = {}
+            time, message_type, *messages = line.split(" - ")
+            logs_dict["time"] = time
+            logs_dict["log_type"] = message_type
+            for message in messages:
+                key, value = message.split(":")
+                value = value.strip()  # stripe out space if any
+                logs_dict[key] = int(value) if value.isnumeric() else value  # convert to number
+
+            log_list.append(logs_dict)
+    return JSONResponse(log_list)
 
 
 def ackermann(m, n):
